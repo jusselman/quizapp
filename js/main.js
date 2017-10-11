3a2e900fb18d2ca313a5118d3426e8f66c08ca6d
jusselman <usselmanjosh@gmail.com>
2017-10-10 22:43:34 -0700

    quizapp

---
+++ js/main.js
@@ -0,0 +1,72 @@
+// array of questions and answers
+var questions = [
+	{
+		question: "What's the capital of Mallorca?",
+		answer: "Palma"
+	},
+	{
+		question: "How many branches of government are there?",
+		answer: 3
+	},
+	{
+		question: "How long does PI go on for?",
+		answer: "Forever"
+	},
+	{
+		question: "What's 9 * 12?",
+		answer: 108
+	},
+	{
+		question: "How many have been evacuated due to forest fires?",
+		answer: 20000
+	},
+
+];
+
+// loop through each questions
+for(var i = 0; i < questions.length; i++) {
+	// store each question value
+	var question = questions[i].question;
+	// store each element to add question too
+	var el = document.getElementById('question' + [i]);
+
+	// check what question and element is being selected
+	console.log(question, el);
+
+	// update text content of each element
+	el.textContent = question;
+}
+
+// check results when the user submits quiz
+function testResults() {
+	// store variables to update correct and incorrect totals
+	var correct = 0;
+	var incorrect = 0;
+
+	// loop through questions array
+	for(var i = 0; i < questions.length; i++) {
+		// store each correct answer
+		var answer = questions[i].answer;
+		// store each user answer
+		var guess = document.getElementById('answer' + [i]).value;
+		// store element to add a class if correct or incorrect
+		var questionSpot = document.getElementById('question' + [i]);
+
+		// check if the user answer matches the correct answer
+		if(answer == guess) {
+			// update class on questionSpot
+			questionSpot.className = 'correct';
+			// add one to correct
+			correct++;
+		} else {
+			// update class on questionSpot
+			questionSpot.className = 'incorrect';
+			// add one to incorrect
+			incorrect++;
+		};
+	};
+
+	// update correct and incorrect values
+	document.getElementById('correct').textContent = correct;
+	document.getElementById('incorrect').textContent = incorrect;
+}
